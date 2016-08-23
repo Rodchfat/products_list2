32159fb930b82a66852c6e7395af7b21612f4c2f
Rod c <raadce@gmail.com>
2016-08-23 23:43:05 +0000

    adding destroy action and fixing the html of the product_index

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -23,7 +23,15 @@ class ProductsController < ApplicationController
   end
 
   def edit
+    @product = Product.find(params[:id])
   end
+  
+  def destroy
+   @product = Product.find(params[:id])
+   @product.destroy
+   redirect_to products_path
+  end
+  
   
   
   private
